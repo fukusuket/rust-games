5e139697f1898fbfcf7542b9763d0c86cf216722
fukusuket <41001169+fukusuket@users.noreply.github.com>
2022-10-12 23:06:01 +0900

    update.

--- ushi/src/main.rs
+++ ushi/src/main.rs
@@ -1,2 +1,12 @@
+extern crate core;
+
+use std::fs;
+
 fn main() {
+    let file = fs::read_to_string("./ushi/cows.txt");
+    let content = match file {
+        Ok(f) => f,
+        Err(error) => panic!("There was a problem opening the file: {:?}", error)
+    };
+    println!("{}", content);
 }
