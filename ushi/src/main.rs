65d692a5b5d60c1dd863e6f5eae3beb1c5ac88b7
fukusuket <41001169+fukusuket@users.noreply.github.com>
2022-10-12 23:47:07 +0900

    show cows ascii art rondomly.

--- ushi/src/main.rs
+++ ushi/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::{fs, io};
+use std::process::Command;
 use rand::Rng;
 use regex::Regex;
 
@@ -6,11 +7,15 @@ fn main() {
     let file = fs::read_to_string("./ushi/cows.txt");
     let content = match file {
         Ok(f) => f,
-        Err(error) => panic!("There was a problem opening the file: {:?}", error)
+        Err(_) => fs::read_to_string("../../ushi/cows.txt").unwrap()
     };
     let r = Regex::new(r"\n\n").unwrap();
     let cows: Vec<&str> = r.split(content.as_str()).collect();
     let mut rng = rand::thread_rng();
-    let x = rng.gen_range(0..10);
-    println!("{}", cows[x]);
+    loop {
+        let x = rng.gen_range(0..cows.len() - 1);
+        println!("{}", cows[x]);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+    }
 }
